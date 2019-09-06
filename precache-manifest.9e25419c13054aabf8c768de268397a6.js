4a5728466caa01e8bd548dbbdb6d3299e6941311
iitsliiz <emtuitt@gmail.com>
2019-09-06 16:44:02 -0400

    Updates

---
+++ precache-manifest.9e25419c13054aabf8c768de268397a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b25e49aec1809a331e87b0a0eac97c73",
+    "url": "/React-Rest/index.html"
+  },
+  {
+    "revision": "ffa6bdef00edc54db916",
+    "url": "/React-Rest/static/css/main.857b4b13.chunk.css"
+  },
+  {
+    "revision": "2790c44532533aa1f119",
+    "url": "/React-Rest/static/js/2.2668f2aa.chunk.js"
+  },
+  {
+    "revision": "ffa6bdef00edc54db916",
+    "url": "/React-Rest/static/js/main.fea33d2b.chunk.js"
+  },
+  {
+    "revision": "d4d09828a73ed3e3cdc4",
+    "url": "/React-Rest/static/js/runtime~main.650affd4.js"
+  }
+]);
